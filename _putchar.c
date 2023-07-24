c99cbd66bf18e4cd876842bf97088293bb1f93be
Zakaria Touras <zakari4touras@gmail.com>
2023-07-24 11:41:48 -0700

    fix betty _putchar

--- _putchar.c
+++ _putchar.c
@@ -1,3 +1,10 @@
+/**
+ * _putchar - writes the character c
+ * @c: The character to print
+ * Return: On success 1
+ * On error, -1 is returned
+ */
+
 void _putchar(char c)
 {
     write(1, &c, 1);
