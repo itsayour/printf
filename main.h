d69c491e6ee1a05bb83953d69b298a79ae311436
Zakaria Touras <zakari4touras@gmail.com>
2023-07-25 10:54:45 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int handle_string(char *str);
+int _putchar(char c);
+int specifier_handler(char c, va_list args);
+int _printf(const char *format, ...);
+
+#endif
